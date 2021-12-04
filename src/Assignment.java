e577df31b156da1e4b739f234477eba15cd904a6
Andrew Poles <andrewnasief3@gmail.com>
2021-12-04 20:51:34 +0200

    Add files via upload

---
+++ src/Assignment.java
@@ -0,0 +1,17 @@
+public class Assignment{
+    public class Router{
+
+    }
+    public class Semaphore{
+
+    }
+    public class Device{
+
+    }
+    public class Network{
+
+    }
+    public static void main(String []args){
+        System.out.println("andrew");
+    }
+}
