ab4c5e1918d9ca97839c98369b693ef748fa1e6e
dovaaa <davidemadphilip@gmail.com>
2021-12-05 21:32:46 +0200

    Changed Assignment.java to Network for code Implementation

---
+++ src/Network.java
@@ -0,0 +1,22 @@
+
+    class Router{
+
+    }
+
+    class Semaphore{
+
+    }
+
+     class Device{
+
+    }
+
+    public class Network{
+        public static void main(String []args){
+            System.out.println("andrew");
+        }
+
+    }
+
+
+
