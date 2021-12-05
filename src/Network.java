018112539732f154d82b9e30b0aa86657863961f
dovaaa <davidemadphilip@gmail.com>
2021-12-06 01:04:21 +0200

    Bug with sync

--- src/Network.java
+++ src/Network.java
@@ -1,54 +1,152 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
-class Router extends Thread{
-        
+class Router {
+    ArrayList<Device> devicesLoggedIn;
+    Semaphore semaphore;
+
+    Router() {
+        devicesLoggedIn = null;
+        semaphore = null;
     }
 
-    class Semaphore{
-        private int signals = 0;
-        private int bound = 0;
-
-        Semaphore(){
-
-        }
-        public Semaphore(int bound){
-         this.bound=bound;
-        }
-
-        public synchronized void use() throws InterruptedException {
-            while (this.signals==bound) wait();
-            this.signals++;
-            this.notify();
-        }
-
-        public synchronized void release() throws InterruptedException {
-            while (this.signals==0) wait();
-            this.signals--;
-            this.notify();
-        }
-
-        public synchronized int value() {
-            return signals;
-        }
+    Router(Semaphore sem) {
+        this.semaphore = sem;
+        this.devicesLoggedIn = new ArrayList<Device>(sem.getBound());
     }
 
-     class Device extends Thread{
-
+    public boolean LogIn(Device dev) throws InterruptedException {
+        System.out.println(dev + "arrived");
+        semaphore.use();
+        System.out.println(Thread.currentThread().getName() + dev + "Occupied");
+        devicesLoggedIn.add(dev);
+        System.out.println(Thread.currentThread().getName() + dev + "Logged In");
+        return true;
     }
 
-    public class Network{
-        public static void main(String []args){
-            Scanner inp = new Scanner(System.in);
-            System.out.println("What is the number of Wi-Fi connections\n");
-            int connNo = inp.nextInt();
-            System.out.println("What is the number of Devices connecting\n");
-            int devNo = inp.nextInt();
+    public boolean LogOut(Device dev) throws InterruptedException {
+        devicesLoggedIn.removeIf(d -> d == dev);
+        semaphore.release();
+        System.out.println(Thread.currentThread().getName() + dev + "Logged Out");
+        return false;
+    }
 
-            Semaphore sem = new Semaphore(connNo);
-
+    public void performActivity(Device dev) {
+        for (Device d : devicesLoggedIn) {
+            if (d == dev) {
+                System.out.println(Thread.currentThread().getName() + dev + "performing online activity");
+            } else System.out.println(Thread.currentThread().getName() + dev + "not logged in yet");
         }
 
     }
+}
+
+class Semaphore {
+    private int signals;
+    private int bound;
+
+    Semaphore() {
+        signals = 0;
+        bound = 0;
+    }
+
+    public Semaphore(int bound) {
+        this.bound = bound;
+    }
+
+    public synchronized void use() throws InterruptedException {
+        if (this.signals == bound) wait();
+        this.signals++;
+
+    }
+
+    public synchronized void release() throws InterruptedException {
+        if (this.signals == 0) wait();
+        this.signals--;
+        this.notify();
+    }
+
+    public synchronized int getBound() {
+        return bound;
+    }
+
+    public synchronized int value() {
+        return signals;
+    }
+}
+
+class Device extends Thread {
+    String deviceName, type;
+    Router router;
+    Semaphore semaphore;
+
+    Device() {
+        deviceName = "";
+        type = "";
+        semaphore = new Semaphore();
+        router = new Router(semaphore);
+
+    }
+
+    Device(String deviceName, String type, Semaphore sem) {
+        this.deviceName = deviceName;
+        this.type = type;
+        this.router = new Router(sem);
+        semaphore = sem;
+    }
+
+    @Override
+    public void run() {
+        try {
+            router.LogIn(this);
+            router.performActivity(this);
+            router.LogOut(this);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String toString(){
+        return  " Name: "+deviceName +" type: " + type + " ";
+    }
+}
+
+public class Network {
+
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.println("What is the number of Wi-Fi connections");
+        int connNo = inp.nextInt();
+        System.out.println("What is the number of Devices connecting");
+        int devNo = inp.nextInt();
+        inp.nextLine();
+        Semaphore sem = new Semaphore(connNo);
+
+
+        ArrayList<Device> devQue = new ArrayList<>();
+
+        for (int i = 0; i < devNo; i++) {
+            String dev;
+            dev = inp.nextLine();
+            String[] devData = dev.split(" ", 2);
+            if (devData.length != 2) {
+                System.out.println("Wrong input terminating!");
+                System.exit(0);
+            }
+            Device device = new Device(devData[0], devData[1], sem);
+            devQue.add(device);
+        }
+        for (int i = 0; i < devQue.size(); i++) {
+            devQue.get(i).start();
+
+        }
+    }
+
+}
 
 
 
